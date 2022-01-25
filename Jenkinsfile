0c4da58899b1293f0c419d35b67c725b1e15bff8
joel92MM <JOEL_TF_92@HOTMAIL.COM>
2022-01-25 15:32:35 +0000

    edi

---
+++ Jenkinsfile
@@ -0,0 +1,16 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Build') { 
+            steps {
+             sh 'docker build -t hello_world_apache_php .'
+             sh 'docker run -d --rm -p 8089:80 hello_world_apache_php'
+            }
+        }
+        stage('Test'){
+            steps{
+                sh 'wget http://localhost:8089/'
+            }
+        }
+}
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+      
+            <?php
+                echo "<h1>¡Hola, soy Joel Meneses Meneses y estas en mi blog!</h1>";
+                echo " <img src='https://www.blogmojo.de/wp-content/uploads/2016/11/blog-thema-finden-1460x730.jpg.webp' alt='Blog'>";
+
+                
+                
+               ?>
+              
+        </div>
+    </body>
+</html>
